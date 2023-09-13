f78eed96f68e8947f1ac67f4d6977cf56714cae8
kbesseghir <k.besseghir@esi-sba.dz>
2023-09-13 18:50:40 +0100

    final commit

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -19,9 +19,34 @@ class BookspiderSpider(scrapy.Spider):
                     book_url='https://books.toscrape.com/catalogue/'+relative_url
 
                 yield response.follow(book_url,callback=self.parse_book_page)
+        
+        next_page= response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+                if 'catalogue/' in next_page :
+                    next_page_url='https://books.toscrape.com/'+next_page
+                else:
+                    next_page_url='https://books.toscrape.com/catalogue/'+next_page
+
+                yield response.follow(next_page_url,callback=self.parse)
+
 
     def parse_book_page(self,response):
-        
+        table_rows = response.css('table tr')
+        yield{
+            'url': response.url,
+            'titel':response.css('.product_main h1::text').get(),
+            'product_type':table_rows[1].css('td::text').get(),
+            'price_excl_tax':table_rows[2].css('td::text').get(),
+            'price_incl_price':table_rows[3].css('td::text').get(),
+            'tax':table_rows[4].css('td::text').get(),
+            'availibility':table_rows[5].css('td::text').get(),
+            'nmr_reviews':table_rows[6].css('td::text').get(),
+            'description':response.xpath('.//div[@id="product_description"]/following-sibling::p/text()').get(),
+            'category':response.xpath('.//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get(),
+            'stars':response.css('p.star-rating').attrib['class'],
+            'price':response.css('p.price_color::text').get(),
+
+        }
 
         
 
